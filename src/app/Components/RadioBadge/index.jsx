3de2b6303af391db7b7bf202254d37ee17de67d9
ankopylova <akopylova.excessteam@gmail.com>
2020-12-25 17:52:34 +0300

    fix: minor bugs in code

--- src/app/Components/RadioBadge/index.jsx
+++ src/app/Components/RadioBadge/index.jsx
@@ -5,7 +5,7 @@ const RadioBadge = ({selected, onChange, filters}) => {
     return (
         <div className="d-flex justify-content-center mx-auto btn-group-sm">
             {filters.map(i =>
-                <label className={cx("btn_radio", selected === i && "btn_radio_select")}>{i}
+                <label className={cx("btn-radio", selected === i && "btn-radio-select")}>{i}
                     <input className="d-none" type="radio" onChange={() => onChange(i)} checked={selected === i}/>
                 </label>
             )}
